7c1a9923bc786a2ccde2e47d05866e2bee53de43
rifqikhuluki <rifkikuluki09@gmail.com>
2025-12-09 23:59:37 +0700

    Dynamic Variable

---
+++ bin/dynamic.dart
@@ -0,0 +1,13 @@
+void main() {
+  dynamic variable = 100;
+  print(variable);
+
+  variable = 10.5;
+  print(variable);
+
+  variable = "wkwkwkwk";
+  print(variable);
+
+  variable = true;
+  print(variable);
+}
---
+++ bin/hitungluas.dart
@@ -0,0 +1,8 @@
+void main() {
+  double panjang = 10;
+  double lebar = 5;
+
+  var luas = panjang * lebar;
+
+  print(luas);
+}
