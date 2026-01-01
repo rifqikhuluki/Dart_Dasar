660688369cff35116eebdd5fa3a4ee81f39e90de
rifqikhuluki <rifkikuluki09@gmail.com>
2026-01-01 22:26:02 +0700

    Konversi Boolean to String

---
+++ bin/konversi_boolean_to_string.dart
@@ -0,0 +1,10 @@
+void main() {
+  var inputString = 'true';
+
+  var inputBoolean = inputString == 'false';
+
+  var booleanToString = inputBoolean.toString();
+
+  print(inputBoolean);
+  print(booleanToString);
+}
