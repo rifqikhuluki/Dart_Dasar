8a554a0012d431f5ba4562db2941b66853d0ed26
rifqikhuluki <rifkikuluki09@gmail.com>
2026-01-08 20:10:30 +0700

    For In Dart

---
+++ bin/for_in.dart
@@ -0,0 +1,11 @@
+void main() {
+  var names = <String>["Rifqi", "Ikmal", "Khuluqi"];
+
+  // for (var i = 0; 1 < names.length; i++) {
+  //   print(names[i]);
+  // }
+
+  for (var isi in names) {
+    print(isi);
+  }
+}
