b36204e75f7d3db8bb5f011e720d10e13eb862e5
rifqikhuluki <rifkikuluki09@gmail.com>
2026-01-09 00:13:48 +0700

    If Else Dart

---
+++ bin/if_else.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nilai = 75;
+  var absen = 02;
+
+  if (nilai > 75) {
+    print("absen: $absen nilai anda A");
+  } else if (nilai == 75) {
+    print("absen: $absen nilai anda B");
+  } else {
+    print("absen: $absen nilai anda C");
+  }
+}
