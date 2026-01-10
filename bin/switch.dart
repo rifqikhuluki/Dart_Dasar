ae7b63ba95e409ecdf18efa416e49bd139b72fc8
rifqikhuluki <rifkikuluki09@gmail.com>
2026-01-10 18:34:33 +0700

    Switch Dart

---
+++ bin/switch.dart
@@ -0,0 +1,21 @@
+void main() {
+  var nilai = "A";
+
+  switch (nilai) {
+    case "A":
+      print("Anda lulus");
+      break;
+    case "B":
+      print("Anda lulus");
+      break;
+    case "C":
+      print("Anda lulus");
+      break;
+    case "D":
+      print("Anda tidak lulus");
+      break;
+    case "E":
+      print("Anda tidak lulus");
+      break;
+  }
+}
